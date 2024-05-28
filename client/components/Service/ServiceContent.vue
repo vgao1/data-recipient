eb42efe8d0aa1d2bacf10e349d3ad49ec9ab1eda
Victoria Gao <71650633+vgao1@users.noreply.github.com>
2024-05-28 11:31:21 -0400

    Limit Checkbox Selection to ServiceContent Component Scope

--- client/components/Service/ServiceContent.vue
+++ client/components/Service/ServiceContent.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { fetchy } from "@/utils/fetchy";
-
+import { ref } from "vue";
 const props = defineProps(["service", "btnText"]);
 const emit = defineEmits(["refreshServices"]);
+const componentRoot = ref<HTMLElement | null>(null);
 const submitConsent = async () => {
   // Create a new Date object to get the current date and time in UTC
   const now = new Date();
@@ -24,20 +25,23 @@ const submitConsent = async () => {
   }
 
   const consents: Array<Record<string, string>> = [];
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-
-  checkboxes.forEach(function (checkbox) {
-    if ((checkbox as HTMLInputElement).checked) {
-      consents.push({ category: (checkbox as HTMLInputElement).name });
-    }
-  });
+  if (componentRoot.value) {
+    const checkboxes = componentRoot.value.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      const input = checkbox as HTMLInputElement;
+      if (input.checked) {
+        consents.push({ category: input.name });
+      }
+    });
+  }
 
   try {
     const description = props.service.purpose;
     const service_id_str = props.service._id;
+    const data_provider_str = props.service.data_provider;
     const data_recipient_str = props.service.data_recipient;
     await fetchy("/api/consentrecords", "POST", {
-      body: { time, description, service_id_str, data_recipient_str, consents },
+      body: { time, description, service_id_str, data_provider_str, data_recipient_str, consents },
     });
   } catch {
     return;
@@ -46,21 +50,23 @@ const submitConsent = async () => {
 };
 </script>
 <template>
-  <p><u>Terms & Conditions</u></p>
-  <span>TurboTax will store the following pieces of data for 2 years: </span>
-  <form @submit.prevent="submitConsent()">
-    <p><b>Required Data</b></p>
-    <div v-for="fieldName in props.service.required_data" :key="fieldName">
-      <input type="checkbox" :name="fieldName" disabled checked />
-      <label :for="fieldName">{{ fieldName }}</label>
-    </div>
-    <p><b>Optional Data</b></p>
-    <div v-for="fieldName in props.service.optional_data" :key="fieldName">
-      <input type="checkbox" :name="fieldName" checked />
-      <label :for="fieldName">{{ fieldName }}</label>
-    </div>
-    <div class="btn-container">
-      <button type="submit" class="pure-button agree-btn">{{ props.btnText }}</button>
-    </div>
-  </form>
+  <div ref="componentRoot">
+    <p><u>Terms & Conditions</u></p>
+    <span>Data Recipient will store the following pieces of data for 2 years: </span>
+    <form @submit.prevent="submitConsent()">
+      <p><b>Required Data</b></p>
+      <div v-for="fieldName in props.service.required_data" :key="fieldName">
+        <input type="checkbox" :name="fieldName" disabled checked />
+        <label :for="fieldName">{{ fieldName }}</label>
+      </div>
+      <p><b>Optional Data</b></p>
+      <div v-for="fieldName in props.service.optional_data" :key="fieldName">
+        <input type="checkbox" :name="fieldName" checked />
+        <label :for="fieldName">{{ fieldName }}</label>
+      </div>
+      <div class="btn-container">
+        <button type="submit" class="pure-button agree-btn">{{ props.btnText }}</button>
+      </div>
+    </form>
+  </div>
 </template>
